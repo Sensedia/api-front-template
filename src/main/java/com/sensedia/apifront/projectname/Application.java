cacbe8f99c786d5062064c323e2ed5d179690404
Ronaldo Ronie <ronaldo.nascimento@sensedia.com>
2018-04-03 13:43:30 -0300

    adicionando interceptor de log

--- src/main/java/com/sensedia/apifront/projectname/Application.java
+++ src/main/java/com/sensedia/apifront/projectname/Application.java
@@ -2,11 +2,20 @@ package com.sensedia.apifront.projectname;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.handler.MappedInterceptor;
+
+import com.sensedia.apifront.projectname.interfaces.interceptor.LogInterceptor;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+	
+	@Bean
+	public MappedInterceptor requestInterceptorInterceptor() {
+		return new MappedInterceptor(new String[] { "/**" }, new LogInterceptor());
 	}
 }
